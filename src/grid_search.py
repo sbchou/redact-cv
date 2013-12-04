950f4dda667fff51ce4457334b66e7652dc06574
Sophie Chou <sbchou@gmail.com>
2013-12-04 02:18:25 -0500

    with boolean and non boolean versions

--- src/grid_search.py
+++ src/grid_search.py
@@ -5,26 +5,10 @@ import redactometer
 import pandas
 import itertools
 import time
+import numpy as np
 
-def gridsearch(param_grid, train, img_root, censor_type="dark"):
-    """Simple paramter training for redactometer. Not as modular as it could be, and
-    also could be more efficient.
-
-    Parameters
-    ----------
-    param_grid : dictionary of possibly paramater values
-    train : training data, a Pandas DataFrame
-
-    Returns
-    ------
-    argmax: a list, in case we have multiple equivalent optimal paramater combos
-    max_score: maximum correctness score from best parameters
-
-    Example
-    -------
-    See test function below
-
-    """
+def gridsearch(param_grid, train, img_root, metric, censor_type="dark"):
+    """Lorem Ipsum"""
     #no cross-val simple param setting
     train = train[train.redaction_type == censor_type]
 
@@ -35,38 +19,73 @@ def gridsearch(param_grid, train, img_root, censor_type="dark"):
 
     max_score = 0
     argmax = []
+    total_img = len(train[train['redaction_type'] == censor_type])
+    print "number img", total_img
+    print "number combos", len(combos)
 
     for combo in combos:
+        print "combo", combo
+        #boolean
+        if metric == 'is_censored':
+            correct = sum((len(redactometer.censor_dark(img_root + i, **combo)[1]) > 0) \
+               == train.ix[i]['is_censored'] for i in train.index)
+            score = float(correct)/float(total_img)
+
         #are we able to get the right number of blobs?
-        correct = sum(len(redactometer.censor_dark(img_root + i, **combo)[1]) \
+        if metric == 'total_censor':
+            correct = sum(len(redactometer.censor_dark(img_root + i, **combo)[1]) \
                 == train.ix[i]['total_censor'] for i in train.index)
-       #score = percent correct
-        score = correct/len(train[train['redaction_type'] == censor_type])
-        print score
+            score = float(correct)/float(total_img)
+
+        #score = percent correct
+        print "correct", correct
+        print "score", score
+
+        #update
         if score >= max_score:
             max_score = score
-            argmax.append(combo)
-    print time.time()            
+            argmax.append(combo) 
     return argmax, max_score     
 
-def complete_search(training, img_dir, numsteps):
+def complete_search(training, img_dir, metric, start, stop, step):
     #range is inclusive
-    param_range = [x/float(numsteps) for x in range(numsteps + 1)]
+    param_range = np.arange(start, stop, step)
     param_grid = {'min_width_ratio' : param_range,
                 'max_width_ratio': param_range,
                 'min_height_ratio': param_range,
                 'max_height_ratio': param_range}
-    return gridsearch(param_grid, training, img_dir)
+    return gridsearch(param_grid, training, img_dir, metric)
 
-def test_complete():
+def eval(test, params, img_root, metric, censor_type="dark"):
+    """Evaluation results on test set"""
+    score = None
+    total_img = len(test[test['redaction_type'] == censor_type])
+
+    if metric == "is_censored":
+        correct = sum((len(redactometer.censor_dark(img_root + i, **params)[1]) > 0) \
+           == test.ix[i]['is_censored'] for i in test.index)
+        score = float(correct)/float(total_img)
+
+    if metric == "total_censor":
+        correct = sum(len(redactometer.censor_dark(img_root + i, **params)[1]) \
+                    == test.ix[i]['total_censor'] for i in test.index)
+        score = float(correct)/float(len(test[test['redaction_type'] == censor_type]))
+    
+    return score
+
+
+def train_complete(metric):
      training = pandas.DataFrame.from_csv('../data/train_dark.csv', sep="\t")
      img_dir = "../train_dark/"
-     return complete_search(training, img_dir, 5)
+     return complete_search(training, img_dir, metric, 0.1, 0.9, 0.1)
 
-def test():
+def train_grid(metric):
     param_grid = {'min_width_ratio': [0.01, 0.05, 0.10, 0.20], 
-                'max_width_ratio': [0.80, 0.90, 0.95, 1.0], 
+                'max_width_ratio': [0.80, 0.85, 0.90, 0.95], 
                 'min_height_ratio': [0.01, 0.05, 0.10, 0.20],
-                'max_height_ratio': [0.80, 0.90, 0.95, 1.0]}   
+                'max_height_ratio': [0.80, 0.85, 0.90, 0.95]}   
     train = pandas.DataFrame.from_csv('../data/train_dark.csv', sep="\t")
-    return gridsearch(param_grid, train, "../train_dark/") 
+    return gridsearch(param_grid, train, "../train_dark/", metric) 
+
+
+
--- src/redactometer.py
+++ src/redactometer.py
@@ -5,29 +5,10 @@ import matplotlib.pyplot as plt
 import scipy.ndimage as ndimage
 
 
-def censor_dark(img_url, min_width_ratio, max_width_ratio,  min_height_ratio, max_height_ratio):
-    """Return mask with dark censor contours.
+def censor_dark(img_url, min_width_ratio=0.2, max_width_ratio=0.9,  min_height_ratio=0.2, max_height_ratio=0.9):
+    #print min_width_ratio, max_width_ratio, min_height_ratio, max_height_ratio
 
-    Parameters
-    ----------
-    img_url : path to image
-    min_width_ratio : minimum width of censor as ratio to entire width
-    max_width_ratio : max width of censor as ratio to entire width
-    min_height_ratio : minimum height of censor as ratio to entire height
-    max_height_ratio : max height of censor as ratio to entire height
-
-    Returns
-    ------
-    mask : a numpy ndarray of the size of the image, black mask with censored 
-            parts outlined in white    
-   
-    Example
-    -------
-    redactometer.censor_dark('../img/2605160002.png', 0.05, 0.9, 0.005, 0.1)
-    (above are approx good parameters)    
-
-    """
-
+    #print "img url", img_url
     img = cv2.imread(img_url)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     #grayscale the image
@@ -41,17 +22,52 @@ def censor_dark(img_url, min_width_ratio, max_width_ratio,  min_height_ratio, ma
 
     total_height, total_width = img.shape
 
+    censors = []
     for cnt in contours:
         area = cv2.contourArea(cnt)
         x, y, width, height = cv2.boundingRect(cnt)
         if (min_height_ratio * total_height < height < max_height_ratio * total_height) \
             and (min_width_ratio * total_width < width < max_width_ratio * total_width):
-            cv2.drawContours(mask, [cnt], 0, 255, 1)
+            cv2.drawContours(mask, [cnt], 0, 255, 1) 
+            censors.append(cnt)            
+
+    #plt.imshow(mask)
+    #plt.show()
+
+    return mask, censors
+
+
+
+def censor_test(img_url, min_width_ratio=0.2, max_width_ratio=0.9,  min_height_ratio=0.2, max_height_ratio=0.9):
+    #print min_width_ratio, max_width_ratio, min_height_ratio, max_height_ratio
+
+    #print "img url", img_url
+    img = cv2.imread(img_url)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    #grayscale the image
+    plt.gray()
+
+    #im.shape = size of png, as np array
+    mask = np.zeros(img.shape) #black mask in shape of image
+    contours, hier = cv2.findContours(np.array(img), 
+                                      cv2.RETR_LIST, 
+                                      cv2.CHAIN_APPROX_SIMPLE)
+
+    total_height, total_width = img.shape
+
+    censors = []
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        x, y, width, height = cv2.boundingRect(cnt)
+        if (min_height_ratio * total_height < height < max_height_ratio * total_height) \
+            and (min_width_ratio * total_width < width < max_width_ratio * total_width):
+            #cv2.drawContours(mask, [cnt], 0, 255, -1) 
+            censors.append(cnt)            
 
     plt.imshow(mask)
     plt.show()
 
-    return mask
+    return mask, censors
 
 
 def template_match(img_url, template_url, outfile_name, threshold):
@@ -72,33 +88,4 @@ def template_match(img_url, template_url, outfile_name, threshold):
         cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
 
     cv2.imwrite(outfile_name, img_rgb)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
