87e3afa35290b32de8f009cfb2245ac25d7c82f3
Sophie Chou <sbchou@gmail.com>
2014-01-30 22:06:03 -0500

    add bounding box drawing helper function and reorganize code

--- src/redactometer.py
+++ src/redactometer.py
@@ -44,7 +44,113 @@ def unpaper(input_dir, output_dir):
             .format(output_dir=output_dir, base=os.path.splitext(os.path.basename(f))[0]))
         os.system("rm {output_dir}*.ppm".format(output_dir=output_dir))
  
+def draw_bounding(img_url, n):
+    """Given input image, draw bounding rectangles on top of
+    first n contours"""
+    img = cv2.imread(img_url)    
+    original = cv2.imread(img_url)
 
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    _, img = cv2.threshold(img, 127, 255, 1)
+    plt.gray()
+
+    contours, hier = cv2.findContours(np.array(img), 
+                                      cv2.RETR_EXTERNAL, 
+                                      cv2.CHAIN_APPROX_SIMPLE)
+    if len(contours) == 1: 
+        print cv2.boundingRect(contours[0])
+        cv2.drawContours(img, [contours[0]], 0, 0, 100) 
+
+    contours, hier = cv2.findContours(np.array(img), 
+                                      cv2.RETR_EXTERNAL, 
+                                      cv2.CHAIN_APPROX_SIMPLE)
+    #sort contours by maximum area
+    contours = sorted(contours, key=lambda cnt:cv2.contourArea(cnt), reverse=True)
+     
+    for cnt in contours[:n]:
+        x,y,w,h = cv2.boundingRect(cnt)
+        cv2.rectangle(original, (x,y), (x+w,y+h), (0,255,0),2)
+
+    plt.imshow(original)
+    plt.show()
+
+def blurred_detection(img_url, aperture):
+    """Return the top left corner form of tuple (x, y) where x is xmin, y is ymin
+    of the contours of n-largest areas, 
+    after running image through a median blur filter with aperture (odd number)"""
+    img = cv2.imread(img_url)
+
+    original = cv2.imread(img_url)
+    img = cv2.medianBlur(original, aperture)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    _, img = cv2.threshold(img, 127, 255, 1)
+
+    plt.gray()
+
+    contours, hier = cv2.findContours(np.array(img), 
+                                      cv2.RETR_EXTERNAL, 
+                                      cv2.CHAIN_APPROX_SIMPLE)
+
+    if len(contours) == 1: 
+        #print cv2.boundingRect(contours[0])
+        cv2.drawContours(img, [contours[0]], 0, 0, 100) 
+
+    contours, hier = cv2.findContours(np.array(img), 
+                                      cv2.RETR_EXTERNAL, 
+                                      cv2.CHAIN_APPROX_SIMPLE)
+    total_height, total_width = img.shape
+
+    censors = []
+    colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (0, 255, 255), (255, 0, 255), (255, 255, 0)]
+
+    contours = sorted(contours, key=lambda cnt:cv2.contourArea(cnt), reverse=True)
+
+    #simply get average minimum area of censors after spotting and make that the cutoff
+    for cnt in contours:    
+        x, y, w, h = cv2.boundingRect(cnt)  
+        area = cv2.contourArea(cnt)
+        #if (100 < x < 750) and (100 < y < 870) \
+        #    and area > 300:
+        if 50 < y and area > 500:
+            cv2.drawContours(original, [cnt], 0, colors[len(censors) % len(colors)], -1) 
+            censors.append((x, y))            
+
+    #plt.imshow(original)
+    #plt.show()
+
+    return censors, original 
+
+
+def batch_blurred_detection(in_dir, out_dir, aperture):
+    """Run the blurred detection on all images in a folder"""
+    imgs = glob.glob(in_dir + '*') #glob ALL THE IMAGES
+
+    results = []
+
+    for img in imgs:
+        name = os.path.basename(img)
+        censors, original = blurred_detection(img, aperture)
+
+        plt.imshow(original)
+        plt.savefig(out_dir + name)
+        plt.close()
+        
+        for censor in censors:
+            results.append((name, censor[0], censor[1]))
+    
+    results = pandas.DataFrame(results, columns=["img", "x", "y"])
+    return(results)
+
+def plot_comp(img1, img2):
+    """Plot two images side by side"""
+    fig = plt.figure()
+    a=fig.add_subplot(1,2,1)
+    imgplot = plt.imshow(img1)
+    a=fig.add_subplot(1,2,2)
+    imgplot = plt.imshow(img2)
+    plt.show()
+
+#### Initial Paramters #############################################
 def censor_fill(img_url, min_width_ratio=0.2, max_width_ratio=0.9,  \
     min_height_ratio=0.2, max_height_ratio=0.9, offset_x=0.05, offset_y=0.10):
     #print min_width_ratio, max_width_ratio, min_height_ratio, max_height_ratio
@@ -52,22 +158,18 @@ def censor_fill(img_url, min_width_ratio=0.2, max_width_ratio=0.9,  \
     #print "img url", img_url
     img = cv2.imread(img_url)
 
-    orig_img = cv2.imread(img_url)
+    original = cv2.imread(img_url)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     #threshold assigns pixel > value to color
     _, img = cv2.threshold(img, 127, 255, 1)
-
     
     #grayscale the image
     plt.gray()
 
-
     #lines = cv2.HoughLinesP(img4, 1, math.pi / 4.0, 100, None, 100, 10)
-
     contours, hier = cv2.findContours(np.array(img), 
                                       cv2.RETR_EXTERNAL, 
                                       cv2.CHAIN_APPROX_SIMPLE)
-
     #print len(contours)
     if len(contours) == 1: 
         #print cv2.boundingRect(contours[0])
@@ -113,10 +215,10 @@ def censor_fill(img_url, min_width_ratio=0.2, max_width_ratio=0.9,  \
                 hull_area = cv2.contourArea(hull) 
                 solidity = float(area)/hull_area
 
-                cv2.drawContours(orig_img, [cnt], 0, colors[len(censors) % len(colors)], -1) 
+                cv2.drawContours(original, [cnt], 0, colors[len(censors) % len(colors)], -1) 
                 censors.append(cnt)            
 
-    return censors, orig_img
+    return censors, original
 
 def batch_censor_dark(source, destination, params):
     """Run the censor detection using given parameters on all images
@@ -127,14 +229,18 @@ def batch_censor_dark(source, destination, params):
     imgs = glob.glob(source + '*') #glob ALL THE IMAGES
     for img in imgs:
         name = os.path.basename(img)
-        censors, orig_img = censor_fill(img, **params)
+        censors, original = censor_fill(img, **params)
         results.append((name, len(censors)))
-        plt.imshow(orig_img)
+        plt.imshow(original)
         plt.savefig(destination + name)
         plt.close()
         print "saved figure : " + name
     return results
-    
+
+
+
+
+#### Future work ################################################
 def template_match(img_url, template_url, outfile_name, threshold):
     """
     For input img found in img_url, and template image found at
@@ -153,82 +259,5 @@ def template_match(img_url, template_url, outfile_name, threshold):
         cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
 
     cv2.imwrite(outfile_name, img_rgb)
-
-
-def blurred_detection(img_url, aperture):
-    """Return the top left corner form of tuple (x, y) where x is xmin, y is ymin
-    of the contours of n-largest areas, 
-    after running image through a median blur filter with aperture (odd number)"""
-    img = cv2.imread(img_url)
-
-    orig_img = cv2.imread(img_url)
-    img = cv2.medianBlur(orig_img, aperture)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    _, img = cv2.threshold(img, 127, 255, 1)
-
-    plt.gray()
-
-    contours, hier = cv2.findContours(np.array(img), 
-                                      cv2.RETR_EXTERNAL, 
-                                      cv2.CHAIN_APPROX_SIMPLE)
-
-    if len(contours) == 1: 
-        #print cv2.boundingRect(contours[0])
-        cv2.drawContours(img, [contours[0]], 0, 0, 100) 
-
-    contours, hier = cv2.findContours(np.array(img), 
-                                      cv2.RETR_EXTERNAL, 
-                                      cv2.CHAIN_APPROX_SIMPLE)
-    total_height, total_width = img.shape
-
-    censors = []
-    colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (0, 255, 255), (255, 0, 255), (255, 255, 0)]
-
-    contours = sorted(contours, key=lambda cnt:cv2.contourArea(cnt), reverse=True)
-
-    #simply get average minimum area of censors after spotting and make that the cutoff
-    for cnt in contours:    
-        x, y, w, h = cv2.boundingRect(cnt)  
-        area = cv2.contourArea(cnt)
-        #if (100 < x < 750) and (100 < y < 870) \
-        #    and area > 300:
-        if 50 < y and area > 500:
-            cv2.drawContours(orig_img, [cnt], 0, colors[len(censors) % len(colors)], -1) 
-            censors.append((x, y))            
-
-    #plt.imshow(orig_img)
-    #plt.show()
-
-    return censors, orig_img 
-
-
-def batch_blurred_detection(in_dir, out_dir, aperture):
-    """Run the blurred detection on all images in a folder"""
-    imgs = glob.glob(in_dir + '*') #glob ALL THE IMAGES
-
-    results = []
-
-    for img in imgs:
-        name = os.path.basename(img)
-        censors, orig_img = blurred_detection(img, aperture)
-
-        plt.imshow(orig_img)
-        plt.savefig(out_dir + name)
-        plt.close()
-        
-        for censor in censors:
-            results.append((name, censor[0], censor[1]))
-    
-    results = pandas.DataFrame(results, columns=["img", "x", "y"])
-    return(results)
-
-def plot_comp(img1, img2):
-    """Plot two images side by side"""
-    fig = plt.figure()
-    a=fig.add_subplot(1,2,1)
-    imgplot = plt.imshow(img1)
-    a=fig.add_subplot(1,2,2)
-    imgplot = plt.imshow(img2)
-    plt.show()
 
 
